93661763f19efa2d2cc1f14fff09b6f9a9f65874
SarahAbuirmeileh <sarah2672003@gmail.com>
2023-05-05 20:12:06 +0300

    enable the start buttun when choose a coffee type

--- src/main/java/com/mycompany/coffeemachineproject/CoffeeMachineGUI.java
+++ src/main/java/com/mycompany/coffeemachineproject/CoffeeMachineGUI.java
@@ -41,7 +41,7 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
         jLabel21 = new javax.swing.JLabel();
         sAmericanoRadioButton = new javax.swing.JRadioButton();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        startButton = new javax.swing.JButton();
         jSlider1 = new javax.swing.JSlider();
         jLabel22 = new javax.swing.JLabel();
 
@@ -56,10 +56,25 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
         jLabel16.setText("Americano");
 
         buttonGroup1.add(dAmericanoRadioButton);
+        dAmericanoRadioButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dAmericanoRadioButtonActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(dEspressoRadioButton);
+        dEspressoRadioButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dEspressoRadioButtonActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(sEspressoRadioButton);
+        sEspressoRadioButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sEspressoRadioButtonActionPerformed(evt);
+            }
+        });
 
         jLabel18.setText("Single");
 
@@ -73,6 +88,11 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
         jLabel21.setText("Espresso");
 
         buttonGroup1.add(sAmericanoRadioButton);
+        sAmericanoRadioButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sAmericanoRadioButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
@@ -142,10 +162,12 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Start");
+        startButton.setText("Start");
+        startButton.setEnabled(false);
 
         jSlider1.setMaximum(10);
         jSlider1.setMinimum(1);
+        jSlider1.setValue(5);
 
         jLabel22.setText("Grinde level");
 
@@ -165,7 +187,7 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                        .addComponent(jButton2)
+                        .addComponent(startButton)
                         .addGap(82, 82, 82)
                         .addComponent(jButton1)
                         .addGap(70, 70, 70))
@@ -190,7 +212,7 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
                 .addGap(27, 27, 27)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(startButton))
                 .addGap(25, 25, 25))
         );
 
@@ -211,6 +233,27 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void sEspressoRadioButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sEspressoRadioButtonActionPerformed
+        // TODO add your handling code here:
+        if (sEspressoRadioButton.isSelected())
+            startButton.setEnabled(true);
+    }//GEN-LAST:event_sEspressoRadioButtonActionPerformed
+
+    private void dEspressoRadioButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dEspressoRadioButtonActionPerformed
+        if (dEspressoRadioButton.isSelected())
+            startButton.setEnabled(true);
+    }//GEN-LAST:event_dEspressoRadioButtonActionPerformed
+
+    private void sAmericanoRadioButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sAmericanoRadioButtonActionPerformed
+        if(sAmericanoRadioButton.isSelected())
+            startButton.setEnabled(true);
+    }//GEN-LAST:event_sAmericanoRadioButtonActionPerformed
+
+    private void dAmericanoRadioButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dAmericanoRadioButtonActionPerformed
+        if(dAmericanoRadioButton.isSelected())
+            startButton.setEnabled(true);
+    }//GEN-LAST:event_dAmericanoRadioButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -252,43 +295,19 @@ public class CoffeeMachineGUI extends javax.swing.JFrame {
     private javax.swing.JRadioButton dAmericanoRadioButton;
     private javax.swing.JRadioButton dEspressoRadioButton;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
     private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel19;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel20;
     private javax.swing.JLabel jLabel21;
     private javax.swing.JLabel jLabel22;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
-    private javax.swing.JRadioButton jRadioButton4;
-    private javax.swing.JRadioButton jRadioButton5;
-    private javax.swing.JRadioButton jRadioButton6;
-    private javax.swing.JRadioButton jRadioButton7;
-    private javax.swing.JRadioButton jRadioButton8;
-    private javax.swing.JRadioButton jRadioButton9;
     private javax.swing.JSlider jSlider1;
     private javax.swing.JRadioButton sAmericanoRadioButton;
     private javax.swing.JRadioButton sEspressoRadioButton;
+    private javax.swing.JButton startButton;
     // End of variables declaration//GEN-END:variables
 }
