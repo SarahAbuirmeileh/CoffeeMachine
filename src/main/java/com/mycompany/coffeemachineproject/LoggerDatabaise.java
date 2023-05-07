edc17ccce25da648575134bc6656dc99166b2716
SarahAbuirmeileh <sarah2672003@gmail.com>
2023-05-07 15:32:24 +0300

    logger method

--- src/main/java/com/mycompany/coffeemachineproject/LoggerDatabaise.java
+++ src/main/java/com/mycompany/coffeemachineproject/LoggerDatabaise.java
@@ -4,6 +4,21 @@ package com.mycompany.coffeemachineproject;
  *
  * @author ساره
  */
-public class LoggerDatabaise {
+public class LoggerDatabaise implements Logger{
+
+    @Override
+    public void log(String message) {
+        
+    }
     
 }
+
+/*
+    xampp
+    general encoging utf8 general ci
+    String details = '';
+    loggers
+    String query = "insert into loggers (details) values (details)"
+    mysql.execute(query);
+
+*/
